0a56c15bf776031fd979252a6ea70ff56617bbc9
William David Edwards <me@galaxias.nl>
2015-04-08 20:11:56 +0200

    Initial commit

---
+++ src/main/java/nl/galaxias/api/ApiFunction1.java
@@ -0,0 +1,17 @@
+package nl.galaxias.api;
+
+/**
+ * Created by wdele on 08-04-15.
+ */
+public class ApiFunction1 {
+    /**
+     * An amazing function
+     * @param args Just some args
+     * @param number A number
+     * @param another_thing Another thing...
+     */
+
+    public void function(String args, int number, double another_thing) {
+        // do stuff
+    }
+}
